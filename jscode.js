5ca3dfa5dc66ceced25e5ef704014e3a8f2b8b4e
SmileyBoy321 <Ungurkristen@gmail.com>
2020-07-10 20:46:51 +0300

    update

--- jscode.js
+++
@@ -1,7 +0,0 @@
-function displayProjectOnClick() {
- document.getElementById("My_Projects").style.display = "flex";
-}
-
-function showHome() {
- document.getElementById("My_Projects").style.display = "none";
-}
