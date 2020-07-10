c80040e4cf64bd8e69a822fcc93dbdfe05364630
SmileyBoy321 <Ungurkristen@gmail.com>
2020-07-10 19:49:59 +0300

    Website stuff

---
+++ jscode.js
@@ -0,0 +1,7 @@
+function displayProjectOnClick() {
+ document.getElementById("My_Projects").style.display = "flex";
+}
+
+function showHome() {
+ document.getElementById("My_Projects").style.display = "none";
+}
